056660514a0d833da80a1eb3d9a11496eedf359d
Omar iglesk <marloo-_-@hotmail.com>
2016-06-13 15:43:41 +0300

    Create Add a Gig Form

---
+++ MusicianHub/Controllers/GigsController.cs
@@ -0,0 +1,99 @@
+﻿using MusicianHub.Models;
+using MusicianHub.ViewModel;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MusicianHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Gigs
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Gigs/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Gigs/Create
+        public ActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(viewModel);
+        }
+
+        // POST: Gigs/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Gigs/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Gigs/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Gigs/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Gigs/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
