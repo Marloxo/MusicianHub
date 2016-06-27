935dceca4ebb184bddb22ce935dc2b742306afe4
Marloxo <omarsabekz@gmail.com>
2016-06-27 23:37:33 +0300

    Add Artists I'm Following

---
+++ MusicianHub/Controllers/FolloweesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Identity;
+using MusicianHub.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MusicianHub.Controllers
+{
+    public class FolloweesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public FolloweesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var artists = _context.Followings
+                .Where(f => f.FollowerId == userId)
+                .Select(f => f.Followee)
+                .ToList();
+
+            return View(artists);
+        }
+    }
+}
